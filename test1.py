9513502968b1ee6ca405c14d7d39f79569feff49
Shreyanshu952 <shreyanshupachauri0515@gmail.com>
2023-07-30 23:06:10 +0530

    Commit done

--- test1.py
+++ test1.py
@@ -1,5 +1,2 @@
 # This test1.py file is created to understand the pull concept i.e how to pull the file from GITHUB repository to vs code 
 
-pip install keyword
-import keyword
-print(keyword.kwlist)
