20c3163846251538aabf72318252bb5153b154fa
Shreyanshu952 <shreyanshupachauri0515@gmail.com>
2023-07-30 23:47:49 +0530

    commit done

--- test.py
+++ test.py
@@ -5,11 +5,8 @@ import math
 def factorial(a):
   return math.factorial(a)
 
-<<<<<<< HEAD
 def logfunc(a):
   return math.log2(a)
 
-=======
 def sinfunc(a):
   return math.sin(a)
->>>>>>> developer_B
